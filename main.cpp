22d9261820061fb08e48fcba4fd6816b8351fefb
dungTnguyen05 <tridung.190705@gmail.com>
2024-07-25 12:25:10 +0930

    completed

---
+++ main.cpp
@@ -0,0 +1,44 @@
+#include "Player.h"
+#include "Human.h"
+#include "Computer.h"
+#include "Referee.h"
+
+int main() {
+    Referee* ref = new Referee();
+
+    int numHumans;
+    int numComputers;
+
+    cout << "Enter the number of humans: ";
+    cin >> numHumans;
+
+    cout << "Enter the number of computers: ";
+    cin >> numComputers;
+
+    Player** players = new Player*[numHumans + numComputers];
+
+    for (int i = 0; i < numHumans; i++) {
+        string name;
+        cout << "Enter a name: ";
+        cin >> name;
+
+        players[i] = new Human(name);
+        players[i] -> makeMove();
+    }
+
+    for (int i = numHumans; i < numHumans + numComputers; i++) {
+        players[i] = new Computer();
+    }
+
+    for (int i = 0; i < numHumans; i++) {
+        for (int j = i + 1; j < numHumans + numComputers; j++) {
+            if (ref -> refGame(players[i], players[j]) == nullptr) {
+                cout << "It's a Tie" << endl;
+            }
+
+            else {
+                cout << ref -> refGame(players[i], players[j]) -> getName() << " Wins" << endl;
+            }
+        }
+    }
+}
